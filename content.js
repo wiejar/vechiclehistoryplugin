70f7f7e614ee333e85af05e8c02088029b7e3ae5
JarekW <w.jaroslaw@gmail.com>
2017-01-21 19:51:03 +0100

    first working version

---
+++ content.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$('.box-form').append("<div id=\"info\"></div>");
+	var str = "";
+	if(jQuery.cookie('enabled')=="true"){ str = "Tak";}
+		else {str = "Nie";}
+	$('#info')[0].innerHTML = "Plugin włączony: " + str;
+	$('.box-form').append('<button class="btn bg-blue btn-sprawdz" id="deactivateCookie">Deaktywuj</button>');
+	$('.box-form').append('<button class="btn bg-blue btn-sprawdz" id="activateCookie">Aktywuj</button>');
+	jQuery("#deactivateCookie")[0].addEventListener("click", function() {
+		    	jQuery.cookie('enabled', false);
+	    		$('#info')[0].innerHTML = "Plugin włączony: Nie";
+	});
+	jQuery("#activateCookie")[0].addEventListener("click", function() {
+		    	jQuery.cookie('enabled', true);
+	    		$('#info')[0].innerHTML = "Plugin włączony: Tak";
+	});
+
+
+  if(jQuery("#nieZnalezionoHistori") != null 
+  	&& jQuery(document.getElementById('_historiapojazduportlet_WAR_historiapojazduportlet_:data'))[0].value != "" 
+  	&& jQuery(document.getElementById('_historiapojazduportlet_WAR_historiapojazduportlet_:rej'))[0].value != "" 
+  	&& jQuery(document.getElementById('_historiapojazduportlet_WAR_historiapojazduportlet_:vin'))[0].value != "" 
+  	&& jQuery.cookie('enabled')=="true") {
+        setTimeout(function(){ 
+            a = jQuery(document.getElementById('_historiapojazduportlet_WAR_historiapojazduportlet_:data'))[0].value;
+		    b = parseInt(a.substr(0,2))+1;
+		    if(b<10) {
+		      b = "0"+b;
+		    }
+		    jQuery('input.field.data')[0].value = a.replace(a.substr(0,2),b);
+		    jQuery('input.field.data').blur();
+			jQuery.cookie('data', jQuery('input.field.data')[0].value);
+          	jQuery('.btn-sprawdz').trigger('click');
+      }, 750);  
+  }
+});
+
